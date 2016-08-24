e23ff697b1003b6b6704bee1d3cd81d33d3643a3
Zainal Mustofa <zainal@41studio.com>
2016-08-24 13:53:16 +0700

    implement angular

---
+++ app/assets/javascripts/angular/controllers/StateController.js
@@ -0,0 +1,7 @@
+// angular
+//   .module('uin', [])
+//   .controller('StateController',['$scope', '$http', function($scope, $http) {
+//   $http.get("http://localhost:3000/activities.json").success(function(res) {
+//     $scope.datas = res;
+//   });
+// }]);
--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,4 +1,19 @@
 class ActivitiesController < ApplicationController
   before_filter :load_activities, only: [:show, :cus_month_calendars,:view_day]
   before_filter :load_book, only: [:booked]
+
+ def index
+ 	@data 		= MerchantDetail.all.limit(10)
+ 	@state 		= MerchantDetail.limit(50).pluck(:state)
+	
+	respond_to do |format|
+	  format.html # show.html.erb
+	  format.xml  { render :xml => @data }
+	  format.json { render :json => { 
+	  	:data => @data,
+	  	:state => @state 
+	  	}
+	  }
+	end
+ end
 end
