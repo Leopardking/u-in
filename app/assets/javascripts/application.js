d1d724ba67310fc267da8da1d07067345a0b82cd
Eki Susanto <ekisusanto@41studio.com>
2016-08-25 08:14:01 +0700

    create activity page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -56,3 +56,19 @@ window.onload = function() {
   }
   $(".alert").fadeOut(3000)
 };
+
+$(function () {
+  $("#slider4").responsiveSlides({
+      auto: true,
+      pager: false,
+      nav: true,
+      speed: 500,
+      namespace: "callbacks",
+      before: function () {
+        $('.events').append("<li>before event fired.</li>");
+      },
+      after: function () {
+        $('.events').append("<li>after event fired.</li>");
+      }
+    });
+});
