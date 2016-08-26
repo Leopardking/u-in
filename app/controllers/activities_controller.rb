1e43368669a5a8e0cef75c1f7e9d4eab8603214c
Zainal Mustofa <zainal@41studio.com>
2016-08-26 16:00:51 +0700

    fix order

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -3,7 +3,7 @@ class ActivitiesController < ApplicationController
   before_filter :load_book, only: [:booked]
 
   def index
-    @promotions = Promotion.filter(params_query).page(params[:page])
+    @promotions = Promotion.filter(params_query).page(params[:page]).order(discount_percent: :desc, discount_price: :desc, created_at: :asc)
     respond_to do |format|
       format.html
       format.json { render :json => {activities: @promotions, next_page: @promotions.next_page} }
